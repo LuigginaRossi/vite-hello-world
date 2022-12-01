cfe586a9801d62fc60aef6c3d2e3661b060a777c
LuigginaRossi <luigginavaldivia@gmail.com>
2022-12-01 14:58:50 +0100

    stampa  titolo e creazione componente

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<script>
+    import UserData from "./components/UserData.vue";
+
+    export default{
+        components: {UserData},
+        data() {
+            return {
+                
+            }
+        },
+    }
+ 
+</script>
+
+<template>
+    <h1>La mia prima app con Vite!</h1>
+        <!-- Bonus:
+    Creare un secondo componente AppHero, che visualizza un jumbotron. -->
+    <!-- npm i bootstrap -->
+    <UserData></UserData>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/UserData.vue
@@ -0,0 +1,25 @@
+<script>
+    export default {
+        data () {
+            return{
+                name: "Michelangelo",
+                surname: "Da Vinci",
+            }
+        },
+        methods: {
+            fullname(){
+                return this.name + this.surname;
+            }
+        },      
+    }
+</script>
+
+<template>
+    <div>
+        <h3>Benvenuto {{fullname()}}</h3>
+    </div>
+</template>
+
+<style>
+
+</style>
